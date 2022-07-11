36762ebe860f0e21a59a8a583e122a004def4a09
Prakash Kumar Singh <Prakashkumarsingh483@gmail.com>
2022-07-11 10:54:23 +0530

    important

---
+++ LowestCommonAncestor.java
@@ -0,0 +1,53 @@
+Given a Binary Tree with all unique values and two nodes value, n1 and n2. The task is to find the
+lowest common ancestor of the given two nodes. We may assume that either both n1 and n2 are present in the tree or none of them are present.
+
+LCA: It is the first common ancestor of both the nodes n1 and n2 from bottom of tree
+
+Input:
+n1 = 2 , n2 = 3  
+       1 
+      / \ 
+     2   3
+Output: 1
+Explanation:
+LCA of 2 and 3 is 1.
+
+
+Input:
+n1 = 2 , n2 = 3  
+       1 
+      / \ 
+     2   3
+Output: 1
+Explanation:
+LCA of 2 and 3 is 1.
+
+
+
+
+Solution : 
+The idea is we assume that both of thne keys are present if eith of the key is there. 
+
+Assumption : 
+1. if it is same as n1 or n2 return root. 
+2. if one of its subtree's contains n1 and other contains n2. 
+3. if one of the subtree's contains both n1 & n2.
+4. if none of the subtree's contains any of the n1 & n2. 
+
+
+class solution {
+public Node LCA(Node root, int n1 , int n2){
+if(root == null) return null;
+if(root.data == n1 || root.data == n2 ) return root;
+Node lca1 = LCA(root.left,n1,n2);
+Node lca2 = LCA(root.right,n1,n2);
+
+if(lca1 != null && lca2 != null)
+return root;
+
+if(lca1 != null)
+return lca1; 
+else
+return lca2;
+}
+}
